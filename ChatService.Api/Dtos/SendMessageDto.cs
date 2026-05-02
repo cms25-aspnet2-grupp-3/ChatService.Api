288a65ba034810b128ba0c9f52d3d52974733c89
Berrin006 <kizildagliberrin@gmail.com>
2026-05-02 10:31:58 +0200

    saving my ongoing work before updates

---
+++ ChatService.Api/Dtos/SendMessageDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ChatService.Api.Dtos
+{
+    public class SendMessageDto
+    {
+        public int SenderId { get; set; }
+        public int ReceiverId { get; set; }
+
+        [Required]
+        [MaxLength(500)]
+        public string Content { get; set; } = string.Empty;
+    }
+}
